ebf1a23a7052b9ac26110fc20626dcbc63c5e9d9
VladislavPristavko <gnazar.123321@gmail.com>
2023-01-14 15:30:22 +0300

    add PostgreSQL connection and create Tables

--- src/main/java/com/bookshop/data/AuthorService.java
+++ src/main/java/com/bookshop/data/AuthorService.java
@@ -5,7 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.sql.ResultSet;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,17 +18,19 @@ public class AuthorService {
     }
 
     public Map<String,List<Author>> getAuthorsData() {
-        List<Author> authors = jdbcTemplate.query("SELECT * FROM authors ORDER BY last_name",
-                (ResultSet resultSet, int rowNumber) -> {
-            Author author = new Author();
-            author.setId(resultSet.getInt("id"));
-            author.setFirstName(resultSet.getString("first_name"));
-            author.setLastName(resultSet.getString("last_name"));
-            return author;
-                });
-        return authors.stream()
+        return jdbcTemplate.query(
+            "SELECT * FROM authors ORDER BY last_name",
+            (ResultSet resultSet, int rowNumber) ->
+                {
+                    Author author = new Author();
+                    author.setId(resultSet.getInt("id"));
+                    author.setFirstName(resultSet.getString("first_name"));
+                    author.setLastName(resultSet.getString("last_name"));
+                    return author;
+                }
+            ).stream()
                 .collect(Collectors.groupingBy(
-                        author -> String.valueOf(author.getLastName().charAt(0))
-                ));
+                        author -> String.valueOf(author.getLastName().charAt(0)))
+                );
     }
 }
